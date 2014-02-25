d879cf63eb0debb44c616ffc77ae4e67813e28b1
inevitable- <randel.ronquillo@gmail.com>
2014-02-25 07:22:53 -0800

    Modified practice folder

---
+++ src/Calculator.php
@@ -0,0 +1,28 @@
+<? php
+class Calculator2
+{
+
+
+    public function add($x, $y)
+    {
+        return $x + $y;
+    }
+
+    public function divide($x,$y)
+    {
+        return $x / $y;
+    }
+
+    public function multiply($x, $y)
+    {
+        return $x * $y;
+    }
+
+    public function subtract($x, $y)
+    {
+       return $x - $y;
+
+    }
+
+ }
+
